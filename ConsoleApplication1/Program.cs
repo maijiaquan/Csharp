1ae3f07e5b44968343e96e2bdc06cf5fdbd23e25
laiwenbin <ben.lai@jodoinc.com>
2017-11-09 09:40:08 +0800

    按引用传递参数 20171109 9.39

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,20 +1,20 @@
-﻿//C#的几种访问修饰符的区别  2017.11.8
-
-/*
+﻿/*
 已学习内容：
-    C#程序的组成（using关键字、类、main函数等）
-    C#装箱和拆箱的概念
-    C#的几种数据类型（值类型、引用类型、指针类型）
-    C#简单的类型转换（隐式——例如double转int、显式——例如装箱）
-    lvalue和rvalue的概念
-    C#的变量分类
-    C#的常量（常量和const关键字）
-    C#的结构体的使用
-    C#类的几种访问修饰符的区别（public、private、protected、internal）
+    20171108
+        C#程序的组成（using关键字、类、main函数等）
+        C#装箱和拆箱的概念
+        C#的几种数据类型（值类型、引用类型、指针类型）
+        C#简单的类型转换（隐式——例如double转int、显式——例如装箱）
+        lvalue和rvalue的概念
+        C#的变量分类
+        C#的常量（常量和const关键字）
+        C#的结构体的使用
+        C#类的几种访问修饰符的区别（public、private、protected、internal）
 
-    Lua的8种基本类型
+        Lua的8种基本类型
     
-
+    20171109
+        引用的用法
 
 待补充
     类
@@ -30,42 +30,36 @@
 感想和理解：
     C#万物皆类
     Lua是一种轻量小巧的脚本语言
-    
-备注：今天刚刚适应了工作环境，学习时间有限，总结也有限。明天的收货和总结会更多更全面。
+
 
 */
 
 
+//按引用传递参数 20171109 9.39
+
 using System;
 
-
-namespace RectangleApplication
+class SwapClass
 {
-    class Rectangle
+    public void swap(ref int x, ref int y)    //原来的&变成了ref
     {
-        internal double length;
-        internal double width;
+        int temp = x;
+        x = y;
+        y = temp;
+    }
 
-        double GetArea()  //类成员的默认访问修饰符为 private
-        {
-            return length * width;
-        }
-        public void Display()
-        {
-            Console.WriteLine("长度： {0}", length);
-            Console.WriteLine("宽度： {0}", width);
-            Console.WriteLine("面积： {0}", GetArea());
-        }
-    }
-    class ExecuteRectangle
+    static void Main(string [] args)
     {
-        static void Main(string[] args)
-        {
-            Rectangle r = new Rectangle();
-            r.length = 4.5;
-            r.width = 3.5;
-            r.Display();
-            Console.ReadLine();
-        }
+        SwapClass s = new SwapClass();
+
+        int a = 1;
+        int b = 2;
+
+        Console.WriteLine("a = {0}, b = {1}", a, b);
+        s.swap(ref a, ref b);   //传参数也要用ref，这一点跟C++不同
+        Console.WriteLine("a = {0}, b = {1}", a, b);
+
+        Console.ReadKey();
     }
+
 }
