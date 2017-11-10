eeb8bab99282387d550b258871c300f461f68a16
laiwenbin <ben.lai@jodoinc.com>
2017-11-10 09:11:54 +0800

    ...

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,65 +1,37 @@
-﻿/*
-已学习内容：
-    20171108
-        C#程序的组成（using关键字、类、main函数等）
-        C#装箱和拆箱的概念
-        C#的几种数据类型（值类型、引用类型、指针类型）
-        C#简单的类型转换（隐式——例如double转int、显式——例如装箱）
-        lvalue和rvalue的概念
-        C#的变量分类
-        C#的常量（常量和const关键字）
-        C#的结构体的使用
-        C#类的几种访问修饰符的区别（public、private、protected、internal）
-
-        Lua的8种基本类型
-    
-    20171109
-        引用的用法
-
-待补充
-    类
-    运算符
-    判断
-    循环
-    数组
-    字符串
-
-已解决的问题：
-    函数的缺省类型是private
-
-感想和理解：
-    C#万物皆类
-    Lua是一种轻量小巧的脚本语言
-
-
-*/
-
-
-//按引用传递参数 20171109 9.39
-
-using System;
-
-class SwapClass
+﻿using System;
+abstract class Shape
 {
-    public void swap(ref int x, ref int y)    //原来的&变成了ref
+    int a = 1;  //可以有成员变量
+    public void printInfo()
     {
-        int temp = x;
-        x = y;
-        y = temp;
+        Console.WriteLine();
     }
 
-    static void Main(string [] args)
+    public abstract int area();
+}
+
+class Rectangle : Shape
+{
+    private int length;
+    private int width;
+    public Rectangle(int w, int l)
     {
-        SwapClass s = new SwapClass();
+        length = l;
+        width = w;
+    }
+    public override int area()
+    {
+        return width * length;
+    }
+}
 
-        int a = 1;
-        int b = 2;
-
-        Console.WriteLine("a = {0}, b = {1}", a, b);
-        s.swap(ref a, ref b);   //传参数也要用ref，这一点跟C++不同
-        Console.WriteLine("a = {0}, b = {1}", a, b);
-
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        Rectangle rect = new Rectangle(10, 5);
+        double a = rect.area();
+        Console.WriteLine("area = {0}", a);
         Console.ReadKey();
     }
-
 }
