c6b19a0f7775f8a7bfa9e28dd5636304a04976bc
laiwenbin <ben.lai@jodoinc.com>
2017-11-11 17:21:14 +0800

    observer模式——业务逻辑1：【出版社一旦出版最新的期刊，订阅者就去购买】

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,25 +1,50 @@
 ﻿using System;
 
-class Publisher
+//个感：这个感兴趣的内容列表 一定是 跟 subject被监控对象有关的
+public class PublishEventArgs : EventArgs
 {
-    public delegate void PublishEventHandler();
-    public event PublishEventHandler OnPublish;
-
-    public void issue() //触发事件的函数
+    public string magazine_name;
+    public PublishEventArgs()   
     {
-        if (OnPublish != null)
-        {
-            Console.WriteLine("publish");
-            OnPublish();  //触发事件
-        }
+
+    }
+    public PublishEventArgs(string name)
+    {
+        magazine_name = name;
     }
 }
 
-class Subscriber
+//subject
+public class Publisher
 {
-    public static void recv()
+    public delegate void PublishEventHandler(object sender, PublishEventArgs e);    //理解2个参数：这两个参数是参照observer的回调函数而加入的
+    public event PublishEventHandler Publish;   //事件是对委托的封装
+
+    //如果被监控对象有需要被访问的成员变量，这里就加上相应的成员变量
+
+    //该函数实现了事件的触发
+    public virtual void OnPublish(PublishEventArgs e)   //理解e：因为委托有e，所以要传e进去，因为自己就是sender，所以不用传this
     {
-        Console.WriteLine("receive");
+        if (Publish != null)    //事件注册了 对象
+        {
+            this.Publish(this, e);  //其实就是调用委托，由于委托可以叠加，因此这一步会调用所有注册对象的方法（即 回调函数/事件处理程序）
+        }
+    }
+
+    //为什么还要额外通过这个函数实现事件的触发呢？暂定是规范的写法
+    public void issue(string magazine_name)
+    {
+        OnPublish(new PublishEventArgs(magazine_name));
+    }
+}
+
+//observer
+public class MrMing
+{
+    //理解两个参数的目的：因为observer对e感兴趣; 因为把subject传进去可以方便直接访问，增强灵活性
+    public static void recv(object sender, PublishEventArgs e)
+    {
+        Console.WriteLine("小明已订阅" + e.magazine_name);
     }
 }
 
@@ -28,9 +53,10 @@ class TestClass
     static void Main(string[] args)
     {
         Publisher publisher = new Publisher();
-        publisher.OnPublish += Subscriber.recv;
-        //publisher.OnPublish += new Publisher.PublishEventHandler(Subscriber.recv);
-        publisher.issue();
+        publisher.Publish += MrMing.recv;   //理解事件的+=：事件是委托的封装，事件的叠加本质上是委托的叠加
+        //publisher.Publish += new Publisher.PublishEventHandler(MrMing.recv);
+        publisher.issue("playboy"); //publisher是触发事件的对象
+
         System.Console.ReadKey();
     }
 }
